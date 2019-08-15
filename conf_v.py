828749b7fc9247c0b9055e7cbbc30ea5a9cf24a6
ai3 <aoii103@126.com>
2019-08-15 16:16:16 +0800

    update

--- conf_v.py
+++ conf_v.py
@@ -2,7 +2,7 @@ class Config:
 
     name = ""
 
-    key = "169d2dd7829fe45690fabec812d05bc3"
+    key = ""
     ua_list = [
         "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/535.1 (KHTML, like Gecko) Chrome/14.0.835.163 Safari/535.1",
         "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:6.0) Gecko/20100101 Firefox/6.0",
